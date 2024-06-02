bbf2e32137e9a960a6777a76212bf12b850d5323
fuyukawasann <hanbyeol8378@hanyang.ac.kr>
2024-06-02 15:31:39 +0900

    Build delete background module and test it at main.py

--- build/240602/delete_bg.py
+++ build/240602/delete_bg.py
@@ -10,3 +10,35 @@
 import cv2
 import numpy as np
 import os
+
+
+class delete_bg:
+    def __init__(self, original_img, handwritten_img):
+        # important!! -> input must be the path of the image file
+        self.original_img = original_img
+        self.handwritten_img = handwritten_img
+        
+    def delete_background(self):
+        # Read the image
+        self.original_img = cv2.imread(self.original_img)
+        self.handwritten_img = cv2.imread(self.handwritten_img)
+        
+        # Convert BGR to RGB(CV2 format to numpy format)
+        ## We only need to convert handwritten image
+        handwritten = cv2.cvtColor(self.handwritten_img, cv2.COLOR_BGR2RGB)
+        
+        # Get Different between Original and Handwritten Image
+        diff = cv2.bitwise_not(cv2.absdiff(self.original_img, self.handwritten_img))
+        diff_rgb = cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
+        
+        # make mask
+        mask = (diff_rgb > 225).all(axis=2)
+        
+        # apply mask
+        handwritten[mask] = [255, 255, 255]
+        
+        # Convert RGB to BGR(numpy format to CV2 format)
+        handwritten = cv2.cvtColor(handwritten, cv2.COLOR_RGB2BGR)
+        
+        # return the value
+        return handwritten
---
+++ build/240602/main.py
@@ -0,0 +1,48 @@
+########## Project Description ##########
+# This is a python script for our project.
+# Required Library: opencv, torch, skimage, numpy, os
+# BUILD: Jun 02, 2024 (KST)
+##########################################
+
+## import the necessary library
+import sys
+import os
+import cv2
+import time
+
+## import my own module
+from delete_bg import delete_bg as del_bg
+
+
+
+
+
+## main part of the file
+if __name__ == '__main__':
+    print("This is the main part of the file")
+    time.sleep(2) # wait for 2 seconds
+    
+    ## Test delete_bg module!!
+    print("Test delete_bg module!!")
+    time.sleep(1) # wait for 1 seconds
+    original_img_path = 'Compare/new_compare_original.jpg'
+    handwritten_img_path = 'Compare/new_compare_handwrite.jpg'
+    del_bg_obj = del_bg(original_img_path, handwritten_img_path)
+    result = del_bg_obj.delete_background()
+    print("End of the module!!")
+    time.sleep(1) # wait for 1 seconds
+    
+    ## Save the result
+    print("Save the result!!")
+    time.sleep(2) # wait for 2 seconds
+    save_DIR = 'Result'
+    if not os.path.exists(save_DIR):
+        os.makedirs(save_DIR)
+    name = input("Enter the name of the file: ")
+    cv2.imwrite(f'{save_DIR}/{name}.jpg', result)
+    print("Complete to save the result!!")
+    time.sleep(1) # wait for 1 seconds
+    
+    ## EOF
+    print("End of the file!!")
+  
