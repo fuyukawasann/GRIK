22ab3a2358047f7ee87df019f9befaa0a0e5dae9
fuyukawasann <hybyeol8378@hanyang.ac.kr>
2024-06-02 09:55:21 +0900

    building start delete_bg.py

---
+++ build/240602/delete_bg.py
@@ -0,0 +1,12 @@
+########## Project Description ##########
+# This is a python script for deleting the background of the handwritten image.
+# Input: Original Image and Handwritten Image
+# Output: Handwritten Image without background
+# Required Library: OpenCV(install required), numpy(install required), os(pre-installed)
+# BUILD: Jun 02, 2024 (KST)
+##########################################
+
+# Import the necessary library
+import cv2
+import numpy as np
+import os
