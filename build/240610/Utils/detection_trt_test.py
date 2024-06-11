e52368da666310fc23ccc663cffe6a8f556c91b7
fuyukawasann <hanbyeol8378@hanyang.ac.kr>
2024-06-11 14:18:44 +0900

    Add GDOWN

--- build/240610/Utils/detection_trt_test.py
+++ build/240610/Utils/detection_trt_test.py
@@ -63,8 +63,8 @@ class detection_trt:
                 self.outputs.append(binding)
         
     def inference(self, input_image):
-        image = input_image.transpose(0, 3, 1, 2)
-        image = np.ascontiguousarray(image)
+        #image = input_image.transpose(0, 3, 1, 2)
+        #image = np.ascontiguousarray(image)
         cuda.memcpy_htod(self.inputs[0]["allocation"], image)
         self.context.execute_v2(self.allocations)
         for o in range(len(self.outputs)):
@@ -84,47 +84,3 @@ class detection_trt:
         test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2RGB)
         result = self.inference(test_image)
         print(result)
-
-    # def load_engine(engine_path):
-    #     TRT_LOGGER = trt.Logger(trt.Logger.INFO)
-    #     with open(engine_path, "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
-    #         return runtime.deserialize_cuda_engine(f.read())
-
-
-    # def allocate_buffers(engine):
-    #     inputs, outputs, bindings = [], [], []
-    #     stream = cuda.Stream()
-    #     for binding in engine:
-    #         size = trt.volume(engine.get_binding_shape(binding)) * engine.max_batch_size
-    #         dtype = trt.nptype(engine.get_binding_dtype(binding))
-    #         host_mem = cuda.pagelocked_empty(size, dtype)
-    #         device_mem = cuda.mem_alloc(host_mem.nbytes)
-    #         bindings.append(int(device_mem))
-    #         if engine.binding_is_input(binding):
-    #             inputs.append({'host': host_mem, 'device': device_mem})
-    #         else:
-    #             outputs.append({'host': host_mem, 'device': device_mem})
-
-    #     return inputs, outputs, bindings, stream
-
-
-    # def do_inference(context, bindings, inputs, outputs, stream):
-    #     [cuda.memcpy_htod_async(inp['device'], inp['host'], stream) for inp in inputs]
-    #     context.execute_async_v2(bindings=bindings, stream_handle=stream.handle)
-    #     [cuda.memcpy_dtoh_async(out['host'], out['device'], stream) for out in outputs]
-    #     stream.synchronize()
-    #     return [out['host'] for out in outputs]
-
-
-    # def main():
-    #     engine_path = "../Weights/best_tiny_400_16.trt"
-    #     engine = load_engine(engine_path)
-    #     context = engine.create_execution_context()
-    #     inputs, outputs, bindings, stream = allocate_buffers(engine)
-
-    #     img = np.random.random((640, 640, 3)).astype(np.float16)
-    #     output = do_inference(context, bindings, inputs, outputs, stream)
-    #     print("Inference output: ", output)
-
-    # if __name__ == "__main__":
-    #     main()
---
+++ build/240610/Utils/gdown.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('gdown --id "15WrOYg9Klmt90WYPce5qsXKug4QwgfDC"')
