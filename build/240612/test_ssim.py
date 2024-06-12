71dce408ee9c8e43917f56fd52efdd0b0b32e7d0
fuyukawasann <hanbyeol8378@hanyang.ac.kr>
2024-06-12 17:22:49 +0900

    Add test_ssim.py

---
+++ build/240612/test_ssim.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+from datetime import datetime
+from Utils.ssim_cpu import ssim_cpu as scc
+
+res_name = 'Tester'
+res_name = f'{res_name}_{datetime.now().strftime("%Y%m%d%H%M%S")}'
+
+os.system('gdown --id "15WrOYg9Klmt90WYPce5qsXKug4QwgfDC"')
+print("GDOWN SUCCESS")
+
+video_path = f'Result/{res_name}/Video'
+if not os.path.exists(video_path):
+    os.makedirs(video_path)
+video_path = f'{video_path}/DEMO.mp4'
+shutil.move('DEMO.mp4', video_path)
+
+ssim_obj = scc(video_path, res_name)
+save_img_path, ssim_eval_time = ssim_obj.ssim_cpu_calculation()
+
+print('Saved Complete')
